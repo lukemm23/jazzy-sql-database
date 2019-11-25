134cd0f8d9d3f3fb42f99710feccca337f9a56cb
Dane Schmidt <daneschmidt@gmail.com>
2019-11-25 16:05:31 -0600

    initial commit

--- database.sql
+++ database.sql
@@ -3,3 +3,10 @@ CREATE TABLE "artists" (
     "artist_name" varchar(80) not null,
     "year_born" date
 );
+
+CREATE TABLE "songs" (
+    "title" VARCHAR(80) not null,
+    "length" VARCHAR(20) not null,
+    "date_released" DATE
+
+);
